c28fcc2a1dde333027300abec06bf7fba70fdbf8
wangyiming <wymm0008@gmail.com>
2017-07-11 14:06:23 +0800

    change the way import koa-router

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
-import Router = require('koa-router')
+import * as Router from 'koa-router'
 import url from './constants/url'
 import account from './controllers/account';
 
 const router = new Router();
 
-router.get(url.ACCOUNT_CREATE, account.onCreate);
-router.get(url.ACCOUNT_LOGIN, account.onLogin);
+router.post(url.ACCOUNT_CREATE, account.onCreate);
+router.post(url.ACCOUNT_LOGIN, account.onLogin);
 
 export default router
