d094727a8a4e8f50bd21f7d8e7325adf7747ffab
wangyiming <wymm0008@gmail.com>
2017-07-11 14:30:12 +0800

    add try/catch to msgparser

--- src/middleware/msg-parser.ts
+++ src/middleware/msg-parser.ts
@@ -1,11 +1,17 @@
 import * as Koa from 'koa';
 import Msg from '../constants/msg'
+import Trace from '../utils/trace'
 
 export default function () {
     return async (ctx: Koa.Context, next) => {
         const dat = ctx.request.body['dat'];
         if (dat) {
-            ctx.service.msg = JSON.parse(dat);
+            try {
+                ctx.service.msg = JSON.parse(dat);
+            } catch (e) {
+                Trace.error(e);
+                ctx.service.msg = Msg.create();
+            }
         }
         await next();
     }
