387ecb03cdd63f94786dca05a2efac31ef2ed581
wangyiming <wymm0008@gmail.com>
2017-07-11 14:08:36 +0800

    fix redundancy msg

--- src/constants/msg.ts
+++ src/constants/msg.ts
@@ -1,12 +1,12 @@
 import MsgCode from './msg-code'
 
 export default class Msg {
-    public status: string = '';
-    public mkey: string = '';
+    public status: string;
+    public mkey: string;
 
-    public email: string = '';
-    public username: string = '';
-    public password: string = '';
+    public email: string;
+    public username: string;
+    public password: string;
 
     public static create(status: string = MsgCode.OK): Msg {
         const msg = new Msg();
